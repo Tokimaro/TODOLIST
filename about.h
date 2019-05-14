99e3d9d069f68c35a8f0f0c411e6764744d279fa
Tokimaro <45174024+Tokimaro@users.noreply.github.com>
2019-05-15 02:30:27 +0300

    Create about.h

---
+++ about.h
@@ -0,0 +1,83 @@
+#pragma once
+
+namespace Проект4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для about
+	/// </summary>
+	public ref class about : public System::Windows::Forms::Form
+	{
+	public:
+		about(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~about()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 20.25F));
+			this->label1->Location = System::Drawing::Point(53, 67);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(565, 32);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Эта программа была созданна в 2019 году.";
+			this->label1->Click += gcnew System::EventHandler(this, &about::label1_Click);
+			// 
+			// about
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(668, 178);
+			this->Controls->Add(this->label1);
+			this->Name = L"about";
+			this->Text = L"О программе";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	};
+}
